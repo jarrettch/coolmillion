91bb2178f1d0592d1a8cf511ecc636ad4b2223ff
jarrettch <jarrett.harris@gmail.com>
2013-10-24 18:14:01 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoolMillion::Application.config.secret_key_base = '0c273a5697d2901aab80200ec7883ff834309bf514586df072d515cf5a6a6a13856f015f97ffb20083bdd76eb07db6353e6da318475606cc8b15fc0cee4b813c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoolMillion::Application.config.session_store :cookie_store, key: '_CoolMillion_session'
